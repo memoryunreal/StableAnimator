3bd3eca7fc00be663b4147e3001b284dd48d1535
Francis <66824433+Francis-Rings@users.noreply.github.com>
2024-11-26 15:41:47 +0800

    Add files via upload

---
+++ command_basic_infer.sh
@@ -0,0 +1,18 @@
+CUDA_VISIBLE_DEVICES=0 python inference_basic.py \
+ --pretrained_model_name_or_path="path/checkpoints/SVD/stable-video-diffusion-img2vid-xt" \
+ --output_dir="path/basic_infer" \
+ --validation_control_folder="path/inference/case-1/poses" \
+ --validation_image="path/inference/case-1/reference.png" \
+ --width=576 \
+ --height=1024 \
+ --guidance_scale=3.0 \
+ --num_inference_steps=25 \
+ --posenet_model_name_or_path="path/checkpoints/Animation/pose_net.pth" \
+ --face_encoder_model_name_or_path="path/checkpoints/Animation/face_encoder.pth" \
+ --unet_model_name_or_path="path/checkpoints/Animation/unet.pth" \
+ --tile_size=16 \
+ --overlap=4 \
+ --noise_aug_strength=0.02 \
+ --frames_overlap=4 \
+ --decode_chunk_size=4 \
+ --gradient_checkpointing
