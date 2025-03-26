6bd61c9190f75b5731dd847133874f86adaab40e
memoryunreal <814514103@qq.com>
2025-03-26 08:20:35 +0000

    Add environment installation script

---
+++ env_install.sh
@@ -0,0 +1,3 @@
+pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu124
+pip install torch==2.5.1+cu124 xformers --index-url https://download.pytorch.org/whl/cu124
+pip install -r requirements.txt
